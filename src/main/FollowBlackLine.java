48fd9c0c539edddf142020c0430e4fcd84ce1c64
Stephan <37297965+BlizzarOfficial@users.noreply.github.com>
2018-10-12 08:20:21 +0200

    FollowBlackLine

--- src/main/FollowBlackLine.java
+++ src/main/FollowBlackLine.java
@@ -1,5 +1,6 @@
 package main;
 
+import lejos.hardware.motor.Motor;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
@@ -17,10 +18,8 @@ public class FollowBlackLine implements Behavior {
 
 	public boolean takeControl() {
 		// TODO take control if the sensor returns black
-		colorSensorProvider = colorSensor.getRedMode();
-		colorSensorProvider.fetchSample(sampleColor, 0);
-		System.out.println("sampleColor = " + sampleColor[0]);
-		return sampleColor[0] < 0.5;
+		
+		return true;
 	}
 
 	public void suppress() {
@@ -29,6 +28,43 @@ public class FollowBlackLine implements Behavior {
 
 	public void action() {
 		suppressed = false;
+		int x = 0;
+		while(x != 1) {
+			
+			colorSensorProvider = colorSensor.getRedMode();
+			colorSensorProvider.fetchSample(sampleColor, 0);
+			//System.out.println("sampleColor = " + sampleColor[0]);
+
+			if (sampleColor[0] < 0.15) {
+				System.out.println("Links = " + sampleColor[0]);
+
+				Motor.B.setSpeed(15);
+				Motor.B.forward();
+				
+				Motor.C.setSpeed(25);
+				Motor.C.backward();
+			
+			}
+			else if (sampleColor[0] > 0.12 && sampleColor[0] <= 0.27 ) {
+				System.out.println("Geradeaus = " + sampleColor[0]);
+
+				Motor.B.setSpeed(150);
+				Motor.B.forward();
+				
+				Motor.C.setSpeed(150);
+				Motor.C.forward();
+			} 
+			else if (sampleColor[0] > 0.27) {
+				System.out.println("Rechts = " + sampleColor[0]);
+				Motor.B.setSpeed(15);
+				Motor.B.backward();
+				
+				Motor.C.setSpeed(25);
+				Motor.C.forward();
+			}
+			
+		}
+		
 		// TODO slow down speed
 		/*if (sampleColor[0] == 0) {
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,19 @@
+
 package main;
 
 import lejos.hardware.port.SensorPort;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
+
 public class Main {
 	public static void main(String[] args) {
 		// creating new behaviors
-		Behavior b1 = new DriveForward();
+		//Behavior b1 = new DriveForward();
 //		Behavior b2 = new HitWall(SensorPort.S2);
 		Behavior b3 = new FollowBlackLine(SensorPort.S3);
 		// b5, b3 -- Follow has a higher priority than searching for the line
-		Behavior b4 = new BatteryLow(6.5f);
+		//Behavior b4 = new BatteryLow(6.5f);
 		// priority: low --> high
 		Behavior[] bArray = { b3 };
 		Arbitrator arby = new Arbitrator(bArray);
